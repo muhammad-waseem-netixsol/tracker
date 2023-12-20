0618e1355f50f94b18f12a4199636894aa871076
muhammad-waseem-netixsol <151015897+muhammad-waseem-netixsol@users.noreply.github.com>
2023-12-21 02:11:41 +0500

    Add files via upload

--- src/balance/Balance.jsx
+++ src/balance/Balance.jsx
@@ -1,10 +1,10 @@
 const Balance = ({balance}) => {
     function formatNumber(amount) {
-        if (amount >= 1000000000) {
+        if (amount >= 1000000000 || amount <= -1000000000) {
             return (amount / 1000000000).toFixed(1) + 'B';
-        } else if (amount >= 1000000) {
+        } else if (amount >= 1000000 || amount <= -1000000) {
             return (amount / 1000000).toFixed(1) + 'M';
-        } else if (amount >= 1000) {
+        } else if (amount >= 1000 || amount <= -1000) {
             return (amount / 1000).toFixed(1) + 'k';
         } else {
             return amount.toString();
