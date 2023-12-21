3ef437d5a7e30dd2d73d0a9c1a2b0b8fc8eaaa1d
muhammad-waseem-netixsol <151015897+muhammad-waseem-netixsol@users.noreply.github.com>
2023-12-21 13:44:20 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ console.log(expenses)
       setTextIsValid(false);
       return;
     }
-    if (Number(amount) === 0 || amount === "" || Number(parseFloat(amount)) > -1 && Number(parseInt(amount)) < 1) {
+    if (Number(amount) === 0 || amount === "" || Number(amount) > -0.01 && Number(amount) < 0.01) {
       setAmountIsValid(false);
       return;
     }
@@ -93,7 +93,7 @@ console.log(expenses)
     loaded={particlesLoaded}
     options={{ "fullScreen": true, "background":{ "image":" linear-gradient(19deg, #21D4FD 0%, #B721FF 100%)" }, "particles":{ "number":{ "value":10, "density":{ "enable":true, "value_area":600 } }, "color":{ "value":"#ffffff" }, "shape": { "type": "square", "stroke":{ "width":0, "color":"#000000" }, "polygon":{ "nb_sides":5 } }, "opacity":{ "value":0.25, "random":true, "anim":{ "enable":false, "speed":1, "opacity_min":0.1, "sync":false } }, "size":{ "value":29, "random":true, "anim":{ "enable":false, "speed":2, "size_min":0.1, "sync":false } }, "line_linked":{ "enable":false, "distance":300, "color":"#ffffff", "opacity":0, "width":0 }, "move":{ "enable":true, "speed":0.5, "direction":"top", "straight":true, "out_mode":"out", "bounce":false, "attract":{ "enable":false, "rotateX":600, "rotateY":1200 } } }, "interactivity":{ "detect_on":"canvas", "events":{ "onhover":{ "enable":false, "mode":"repulse" }, "onclick":{ "enable":false, "mode":"push" }, "resize":true }, "modes":{ "grab":{ "distance":800, "line_linked":{ "opacity":1 } }, "bubble":{ "distance":790, "size":79, "duration":2, "opacity":0.8, "speed":3 }, "repulse":{ "distance":400, "duration":0.4 }, "push":{ "particles_nb":4 }, "remove":{ "particles_nb":2 } } }, "retina_detect":true}}
             />
-            <div className="container mx-auto">
+            <div className="container mx-auto px-3">
     
     <div className="max-w-[400px] mx-auto">
       <h1 className="font-semibold text-xl py-5">Expense Tracker</h1>
